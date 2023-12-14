3887a8325c0e12cffed507111d7b66d0a52a0144
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-12-13 21:37:36 -0700

    Security

--- src/main/java/com/cydeo/entity/BaseEntityListener.java
+++ src/main/java/com/cydeo/entity/BaseEntityListener.java
@@ -22,8 +22,7 @@ public class BaseEntityListener extends AuditingEntityListener {
 
         baseEntity.insertDateTime = LocalDateTime.now();
         baseEntity.lastUpdateDateTime = LocalDateTime.now();
-        baseEntity.insertUserId = 1L;
-        baseEntity.lastUpdateUserId = 1L;
+
 
         if (authentication != null && !authentication.getName().equals("anonymousUser")) {
             Object principal = authentication.getPrincipal();
@@ -32,13 +31,13 @@ public class BaseEntityListener extends AuditingEntityListener {
         }
     }
 
+    @PreUpdate
     public void onPreUpdate(BaseEntity baseEntity) {
 
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
 
         baseEntity.lastUpdateDateTime = LocalDateTime.now();
-        baseEntity.lastUpdateUserId = 1L;
 
         if (authentication != null && !authentication.getName().equals("anonymousUser")) {
             Object principal = authentication.getPrincipal();
