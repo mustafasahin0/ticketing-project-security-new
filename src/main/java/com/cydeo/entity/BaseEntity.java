257e83835d22deb441275866d2a40fc351e28f84
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-12-13 21:18:50 -0700

    Security

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -3,6 +3,7 @@ package com.cydeo.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -11,40 +12,21 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Data
 @MappedSuperclass
+@EntityListeners(BaseEntityListener.class)
 public class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
-    @Column(nullable = false,updatable = false)
-    private LocalDateTime insertDateTime;
-    @Column(nullable = false,updatable = false)
-    private Long insertUserId;
+    @Column(nullable = false, updatable = false)
+    protected LocalDateTime insertDateTime;
+    @Column(nullable = false, updatable = false)
+    protected Long insertUserId;
     @Column(nullable = false)
-    private LocalDateTime lastUpdateDateTime;
+    protected LocalDateTime lastUpdateDateTime;
     @Column(nullable = false)
-    private Long lastUpdateUserId;
+    protected Long lastUpdateUserId;
 
-    private Boolean isDeleted=false;
-
-    @PrePersist
-    public void onPrePersist(){
-        this.insertDateTime = LocalDateTime.now();
-        this.lastUpdateDateTime = LocalDateTime.now();
-        this.insertUserId = 1L;
-        this.lastUpdateUserId = 1L;
-    }
-
-    @PreUpdate
-    public void onPreUpdate(){
-        this.lastUpdateDateTime = LocalDateTime.now();
-        this.lastUpdateUserId = 1L;
-    }
-
-
-
-
-
-
+    private Boolean isDeleted = false;
 }
