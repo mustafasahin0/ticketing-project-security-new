6bd87fe16149d309b240b68d90531d95f2314040
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-12-13 21:32:53 -0700

    Security

--- src/main/java/com/cydeo/entity/BaseEntityListener.java
+++ src/main/java/com/cydeo/entity/BaseEntityListener.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @Component
 public class BaseEntityListener extends AuditingEntityListener {
 
-
+    @PrePersist
     public void onPrePersist(BaseEntity baseEntity) {
 
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
